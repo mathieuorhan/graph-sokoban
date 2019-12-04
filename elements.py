cd4873539cc33ba43d94e3362ac9a366309bc380
Mathieu Orhan <mathieu.orhan95@gmail.com>
2019-12-05 00:40:00 +0100

    first commit: from pixels to graph

---
+++ elements.py
@@ -0,0 +1,11 @@
+import torch
+
+
+class TinyWorldElements:
+    WALL = torch.tensor([0, 0, 0], dtype=torch.uint8)
+    FLOOR = torch.tensor([243, 248, 238], dtype=torch.uint8)
+    BOX_TARGET = torch.tensor([254, 126, 125], dtype=torch.uint8)
+    BOX_ON_TARGET = torch.tensor([254, 95, 56], dtype=torch.uint8)
+    BOX = torch.tensor([142, 121, 56], dtype=torch.uint8)
+    PLAYER = torch.tensor([160, 212, 56], dtype=torch.uint8)
+    PLAYER_ON_TARGET = torch.tensor([219, 212, 56], dtype=torch.uint8)
