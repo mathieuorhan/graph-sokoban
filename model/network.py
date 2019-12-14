be8138a8c22046c3398b1d12d43c43ba7631db68
Mathieu Orhan <mathieu.orhan95@gmail.com>
2019-12-14 15:44:27 +0100

    add: super simple GCN model

---
+++ model/network.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn.functional as F
+
+from message_passing import GCNConv
+
+
+class Net(torch.nn.Module):
+    def __init__(self, nodes_features=4):
+        super(Net, self).__init__()
+        self.conv1 = GCNConv(nodes_features, 32)
+        self.conv2 = GCNConv(32, 32)
+        self.conv3 = GCNConv(32, 1)
+
+    def forward(self, x, edge_index):
+        # Apply graph layers
+        x = self.conv1(x, edge_index)
+        x = self.conv2(x, edge_index)
+        x = self.conv3(x, edge_index)
+        return x
