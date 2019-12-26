bdc4db91f0bcb89b5470af6b79c8508b2cc7c9a1
mathieuorhan <mathieu.orhan95@gmail.com>
2019-12-26 16:57:54 +0100

    fix: node to action y/x swapped + indep random gen

--- rl/explore.py
+++ rl/explore.py
@@ -21,18 +21,22 @@ def best_from_nodes(scores, state):
     return idx_best
 
 
-def epsilon_greedy(state, net, eps):
+def epsilon_greedy(state, net, eps, random_generator=None):
     """Epsilon-greedy exploration
     
     Args:
         - state: Data
         - net: policy net
         - eps: epsilon
+        - random_generator
     Return:
-        - node indice corresponding to the best action
+        - node indice corresponding to the action
         - int in [0, ..., 8], action taken 
     """
-    sample = random.random()
+    if random_generator is None:
+        sample = random.random()
+    else:
+        sample = random_generator.random()
     if sample > eps:
         with torch.no_grad():
             scores = net(state.x.to(device), state.edge_index.to(device))
@@ -40,20 +44,26 @@ def epsilon_greedy(state, net, eps):
 
     else:
         # Sample randomly a node among the neighboring nodes of the player
-        player_neighbors = state.edge_index[:, state.edge_index[0] == state.player_idx][1]
+        player_neighbors = state.edge_index[:, state.edge_index[0] == state.player_idx][
+            1
+        ]
         nb_neighbors = player_neighbors.size(0)
         action = random.randint(0, nb_neighbors + 1)
         if action == 0:
             chosen_idx = state.player_idx[0]
         else:
-            chosen_idx = random.choice(player_neighbors)   
-    
+            chosen_idx = random.choice(player_neighbors)
+
     action_node_pos = state.pos[chosen_idx]
     # Whether or not there is a box on the chosen node
     action_node_box = 1 if state.x[chosen_idx][0] == 1 else 0
 
     # 9 possibilities corresponding to the 9 possible actions
     diff_pos = action_node_pos - state.pos[state.player_idx]
-    dx, dy = diff_pos[0, 0].item(), diff_pos[0, 1].item()
+    dy, dx = diff_pos[0, 0].item(), diff_pos[0, 1].item()
 
-    return torch.tensor([[chosen_idx]], dtype=torch.long, device=device), NODES_TO_ACTIONS[(dx, dy, action_node_box)]
+    return (
+        torch.tensor([[chosen_idx]], dtype=torch.long, device=device),
+        NODES_TO_ACTIONS[(dx, dy, action_node_box)],
+    )
+
