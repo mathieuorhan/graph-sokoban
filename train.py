c24941005e84ca5ebe28fd5fa3b803f288834d55
mathieuorhan <mathieu.orhan95@gmail.com>
2020-01-05 16:11:27 +0100

    chg: update train, rm debug

--- train.py
+++ train.py
@@ -1,5 +1,8 @@
-import torch.nn.functional as F
+import pickle
 import time
+import os
+
+import torch.nn.functional as F
 
 import options
 import logger
@@ -16,10 +19,25 @@ logger.setup_logger(opt.logs, training_id=opt.training_id)
 
 # trainer = QLearningTrainer(opt)
 trainer = QLearningGraphCenteredTrainer(opt)
+history = dict()
 
 for epoch in range(opt.epochs):
-    trainer.train_one_epoch()
+    epoch_info = trainer.train_one_epoch()
+    # monitor the information about training
+    for info in epoch_info:
+        if info not in history:
+            history[info] = [epoch_info[info]]
+        else:
+            history[info].append(epoch_info[info])
+
     trainer.eval_one_epoch()
 
     if opt.render and epoch % opt.render_every == 0:
         trainer.render_one_episode(0)
+
+    # Save weights
+    trainer.save_model()
+
+# Saving history in the logs
+with open(os.path.join(opt.logs, opt.training_id, "history.pkl"), "wb") as f:
+    pickle.dump(history, f, pickle.HIGHEST_PROTOCOL)
