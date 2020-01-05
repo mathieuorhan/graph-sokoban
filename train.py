f6d6e81cabbbd798ba71c35c188f5a5a2c072825
mathieuorhan <mathieu.orhan95@gmail.com>
2020-01-05 15:03:35 +0100

    chg: train.py to first working training setup

--- train.py
+++ train.py
@@ -4,27 +4,18 @@ import time
 import options
 import logger
 from rl.qlearning_trainer import QLearningTrainer
-from data.embedding import (
-    MinimalEmbedding,
-    NoWallsEmbedding,
-    NoWallsV2Embedding,
-    DirectionalEmbedding,
-    PositionalEmbedding,
-)
+from rl.qlearning_trainer_gc import QLearningGraphCenteredTrainer
+from data.embedding import DirectionalPositionalEmbedding
 
 opt = options.parse_options()
-opt.embedding = PositionalEmbedding()
-# opt.unet_hidden_channels = 256
-# opt.unet_depth = 10
-# opt.unet_pool_ratios = 0.5
-# opt.unet_sum_res = False
-# opt.unet_act = F.relu
+opt.embedding = DirectionalPositionalEmbedding()
 opt.training_id = str(int(time.time()))
 
 # Setup logger
 logger.setup_logger(opt.logs, training_id=opt.training_id)
 
-trainer = QLearningTrainer(opt)
+# trainer = QLearningTrainer(opt)
+trainer = QLearningGraphCenteredTrainer(opt)
 
 for epoch in range(opt.epochs):
     trainer.train_one_epoch()
