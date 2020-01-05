6e9bbefb9c143ffc1c2d2a48cdecbed857ea7a73
Bastien Dechamps <bastien.dechamps2@gmail.com>
2020-01-05 16:09:25 +0100

    rm: debug.py --> train.py

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
+import torch
 import torch.nn.functional as F
 import time
+import pickle
+import os
 
 import options
 import logger
-from rl.qlearning_trainer import QLearningTrainer
+from model.graph_centered import GraphCenteredNet
 from rl.qlearning_trainer_gc import QLearningGraphCenteredTrainer
 from data.embedding import DirectionalPositionalEmbedding
 
@@ -13,13 +16,25 @@ opt.training_id = str(int(time.time()))
 
 # Setup logger
 logger.setup_logger(opt.logs, training_id=opt.training_id)
-
-# trainer = QLearningTrainer(opt)
+history = dict()
 trainer = QLearningGraphCenteredTrainer(opt)
 
+# Training loop
 for epoch in range(opt.epochs):
-    trainer.train_one_epoch()
+    epoch_info = trainer.train_one_epoch()
+    # monitor the information about training
+    for info in epoch_info:
+        if info not in history:
+            history[info] = [epoch_info[info]]
+        else:
+            history[info].append(epoch_info[info])
+
+    # Evaluation
     trainer.eval_one_epoch()
 
-    if opt.render and epoch % opt.render_every == 0:
-        trainer.render_one_episode(0)
+    # Save weights
+    trainer.save_model()
+
+# Saving history in the logs
+with open(os.path.join(opt.logs, opt.training_id, "history.pkl"), "wb") as f:
+    pickle.dump(history, f, pickle.HIGHEST_PROTOCOL)
